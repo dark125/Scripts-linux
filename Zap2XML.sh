fcf53a45d064a83a359765391b83638a196808f0
dark125 <one1grafik@gmail.com>
2020-04-02 01:55:53 -0500

    Update Zap2XML.sh

--- Zap2XML.sh
+++ Zap2XML.sh
@@ -6,12 +6,12 @@ cd /home/pi/guia/Scripts
 
 #Ejecutar zap2xml.pl
 
-./zap2xml.pl -z -C /home/pi/guia/Scripts/clarotv-sat-peru.txt -D -I -L -T -b
+./zap2xml.pl -z -C /home/pi/guia/Scripts/clarotv-sat-peru.txt -D -I -L -T -b -e
 
 #Combinar egp peru con epg colombia en un archivo llamado claro-combinado.xmltv
 #El nombre de este archivo esta en el archivo clarotv-col.txt
 
-./zap2xml.pl -z -C /home/pi/guia/Scripts/clarotv-col.txt -J /home/pi/guia/temp_epg/claro-per.xmltv -D -I -L -T -b
+./zap2xml.pl -z -C /home/pi/guia/Scripts/clarotv-col.txt -J /home/pi/guia/temp_epg/claro-per.xmltv -D -I -L -T -b -e
 
 #Eliminar los archivos temporales creados
 
